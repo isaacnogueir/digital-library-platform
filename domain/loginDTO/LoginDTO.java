bd9a515388e90810012ddf2da41d3b5b7f3416d5
Isaac <isaac.nogueira@icloud.com>
2025-07-28 21:01:26 -0300

    Remove pastas duplicadas - mantém apenas estrutura correta em src/

--- domain/loginDTO/LoginDTO.java
+++
@@ -1,4 +0,0 @@
-package com.project2025.auth_2.domain.loginDTO;
-
-public record LoginDTO(String login, String password) {
-}
--- domain/loginDTO/LoginResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package com.project2025.auth_2.domain.loginDTO;
-
-import com.project2025.auth_2.domain.user.Role;
-
-public record LoginResponseDTO(String token, Role role) {
-}
